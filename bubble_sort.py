6c10bef75d4b3c28d9371e69eb3acd1f58d835c2
Nasser Tapsoba <nassertapsoba@meta.com>
2026-03-06 16:00:45 -0500

    Refactor the bubble sort.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,22 @@
 a = [5, 3, 8, 6, 2]
+b = [7, 2, 8, 1, 5]
 
 
 def sorting(a_list):
     n = len(a_list)
-    for j in range(n):
-        for i in range(len(a_list) - 1):
+    for j in range(
+        n
+    ):  # nested loop is required to cover every element in the list to make sure the whole list is sorted.
+        for i in range(n - 1):
             if a_list[i] > a_list[i + 1]:
-                temp0 = a_list[i]
-                temp1 = a_list[i + 1]
-                a_list[i] = temp1
-                a_list[i + 1] = temp0
+                a_list[i], a_list[i + 1] = (
+                    a_list[i + 1],
+                    a_list[i],
+                )  # pythonic way to swap between elements in positions i and i + 1.
 
-    return a_list
+    return f"Sorted list: {a_list}"
 
 
 if __name__ == "__main__":
     print(sorting(a))
+    print(sorting(b))
